a39438fda119156ffda3c8ca15d63cdddb617067
Anand kumar <movidianand123@gmail.com>
2024-10-14 16:47:55 +0530

    change done

--- 02.conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "ramesh::hi suresh, how are you"
-echo "suresh::hi ramesh, i am fine what about you"
-echo "ramesh::i am fien suresh whats up anything urgent"
-echo "suresh:: nothing ramesh ijust want to talk to you bro"
-
--- 03.new file.sh
+++ 03.new file.sh
@@ -2,7 +2,7 @@
 person1=ramesh
 person2=suresh
 
-echo "$perosn1::hi $person2, how are you"
+echo "$person1::hi $person2, how are you"
 echo "$person2::hi $person1, i am fine what about you"
 echo "$person1::i am fien $person2 whats up anything urgent"
-echo "$person2:: nothing $person1 ijust want to talk to you bro"
+echo "$person2::nothing $person1 ijust want to talk to you bro"
