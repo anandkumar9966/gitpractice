9ac3bba2400c751f77798de0f95cbaaf380dbffc
Anand kumar <movidianand123@gmail.com>
2024-10-15 16:14:28 +0530

    command line aruguement

---
+++ test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo " the number of arguements passed are $# "
+echo " the script file name is: $0 "
+echo " the first aruguement is: $1 "
+echo " the second aruguement is: $2 "
+echo " the third aruguement is: $3 "
+echo " all aruguements with @: $@ "
+echo " all aruguements with *: $* "
+
