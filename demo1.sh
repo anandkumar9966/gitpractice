f62e6a20d1ae0c3e0bc8bbd17b87c63b593e1512
Anand kumar <movidianand123@gmail.com>
2024-10-15 15:42:41 +0530

    new shell programme1

--- demo1.sh
+++ demo1.sh
@@ -3,7 +3,7 @@ a=10
 b=20
 COURSE="PYTHON"
 ACTION="SLEEPING"
-echo"the values of a and b are:$a and $b "
+echo "the values of a and b are:$a and $b "
 echo " i have taken the course is $COURSE"
 echo " i always like $ACTION "
 
