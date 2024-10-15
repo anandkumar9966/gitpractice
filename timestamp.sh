211b17620ee2baad4706207107bee56ce6e27793
Anand kumar <movidianand123@gmail.com>
2024-10-15 17:50:05 +0530

    log file with time stamp

---
+++ timestamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+timestamp=$(date+%d_%M_%Y%h_%m_%s)
+echo "this is a data to log file" >> ${timestamp}.log
+echo "this is extra to log file " >> ${timestamp}.log
+date >> ${timestamp}.log
+echo >> ${timestamp}.log
+echo "the log file were created successfully"
