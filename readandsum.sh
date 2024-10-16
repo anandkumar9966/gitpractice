07f759116895a4b3b933d896ee5d0104e96fb67d
Anand kumar <movidianand123@gmail.com>
2024-10-16 18:51:55 +0530

    readandsum12

--- readandsum.sh
+++ readandsum.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "enter any 4 digit number:" n
-sum=$(echo $n|cut -c 1)
-sum=$(echo $n|cut -c 2)
-sum=$(echo $n|cut -c 3)
-sum=$(echo $n|cut -c 4)
+a=$(echo $n | cut -c 1)
+b=$(echo $n | cut -c 2)
+c=$(echo $n | cut -c 3)
+d=$(echo $n | cut -c 4)
 sum=$[a+b+c+d]
 echo " the sum of 4 digit number is:"$sum
