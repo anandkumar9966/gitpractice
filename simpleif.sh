51df9a9a27b3752d7fe1effd6dff0dceba9ab9c1
Anand kumar <movidianand123@gmail.com>
2024-10-16 22:41:28 +0530

    simpleifstateme3

--- simpleif.sh
+++ simpleif.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 read -p " pelase enter the name:" name
 
-if [ $name = "sunny" ]
-then
+if [ $name = "sunny" ]; then
      echo " hi sunny how are you"
+else
+     echo "hi guest i know you are not sunny"
 fi
-      echo "good evening young man"
+      echo "good evening guest"
