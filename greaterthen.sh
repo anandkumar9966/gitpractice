01a7fe059d4f3c0e9fd0ba457473d46f461dd995
Anand kumar <movidianand123@gmail.com>
2024-10-16 23:03:42 +0530

    greaterthen

---
+++ greaterthen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "enter first number:" a
+read -p "enter first number:" b
+if [ $a -gt $b ]; then
+echo "$a is graeter than $b"
+else
+echo "$b is greater than $a"
+if [ $a -eq $b ]; then
+echo "both are eqaul"
+else
+echo " both are not equal"
+fi
+
+
