14a6842880f2a559f133b9dc1d25364c27359b87
Anand kumar <movidianand123@gmail.com>
2024-10-16 18:01:20 +0530

    additon and sub programme

--- addition.sh
+++ addition.sh
@@ -2,6 +2,9 @@
 read -p "enter the valu of a:" a
 read -p "enter the value of b:" b
 sum=$((a+b))
+diff=$[a-b]
 echo "the sum of a and b are:$sum"
+echo
+echo "the difference between a and b are:$diff" 
 
 
