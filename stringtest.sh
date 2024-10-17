2f65d08fc261584b2705e0cea6451985cf457f51
Anand kumar <movidianand123@gmail.com>
2024-10-17 23:57:40 +0530

    string test options1

--- stringtest.sh
+++ stringtest.sh
@@ -3,7 +3,7 @@ read -p "enter first string:" str1
 read -p "enter second string:" str2
 if [ $str1 = $str2 ]; then
 echo "both $str1 and $str2 are equal"
-elif [ $str1 \< $ str2 ]; then
+elif [ $str1 \< $str2 ]; then
 echo "$str1 is less than $str2"
 else
 echo "$str1 is greater than $str2"
