0cf0054c868fa389c42b0552c01fbd3785a441ba
Anand kumar <movidianand123@gmail.com>
2024-10-17 15:40:38 +0530

    file checkin32 example

--- filechecking2.sh
+++ filechecking2.sh
@@ -3,9 +3,9 @@ read -p "enter the file name which you want to search for:" fname
 if [ -e $fname ]; then
  if [ -f $fname ]; then
  if [ -s $fname ]; then
- echo " file $fname is not a empty file"
+ echo " file $fname is non empty file"
  else
- echo " file $fname is not a empty file"
+ echo " file $fname is a empty file"
  fi
  else
  echo " file $fname is not a regular file"
