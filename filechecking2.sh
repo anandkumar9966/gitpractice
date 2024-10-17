7175ae99dd8803f072a1b7987b13097fe91d9118
Anand kumar <movidianand123@gmail.com>
2024-10-17 15:26:59 +0530

    file checking2 example

---
+++ filechecking2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -p "enter the file name which you want to search for:" fname
+if [ -e $fname ]; then
+ if [ -f $fname ]; then
+ if [ -s $fname ]; then
+ echo " file $fname is not a empty file"
+ else
+ echo " file $fname is not a empty file"
+ fi
+ else
+ echo " file $fname is not a regular file"
+ fi
+ else
+ echo " file $fname doesnot exist"
+ fi
