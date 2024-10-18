d25310f0cd53e10f428553e6bc2abe27d1c1ddff
Anand kumar <movidianand123@gmail.com>
2024-10-18 17:45:25 +0530

    installing mysq123

--- installmysql.sh
+++ installmysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i=$( id -u )
 if [ $i -ne 0 ]; then
-   echo " please run the script with root user to install mysql"
+   echo " please run the script with root user to install"
    exit 1
 else
    echo " installing mysql server...please wait"
@@ -15,3 +15,10 @@ else
 echo "mysql installation is successful"
 fi 
 
+yum install git -y
+if [ $? -ne 0 ]; then
+echo "git installation is failed please check"
+exit 1
+else
+echo "git installation is successful"
+fi 
