301ed0cabd95210df7bb7c9013fbeef5ce99c0ab
Anand kumar <movidianand123@gmail.com>
2024-10-18 14:04:48 +0530

    installing mysql1

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ id -u -ne 0 ]; then
-echo " please run the script with root user to install mysql"
+i=$(id -u)
+if [ $i -ne 0 ]; then
+   echo " please run the script with root user to install mysql"
 else
-echo " installing the script with root user"
+   echo " installing the script with root user"
 fi
 yum install mysql -y 
