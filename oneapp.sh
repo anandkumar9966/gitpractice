a391e39d1748b76513c0a25cb3aa9cfc22ec6943
Anand kumar <movidianand123@gmail.com>
2024-10-20 00:06:07 +0530

    oneapp

---
+++ oneapp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+while [ true ]
+read -p "enter the file name to display the content:" fname
+if [ -f $fname ]; then
+echo "----------------------------------"
+echo " the content of the $fname:"
+cat $fname
+echo "----------------------------------"
+else
+echo "the $fname is not a regular file or may not exist"
+fi
+read -p "do you want to check the content of the another file [y|n]" option
+case $option in
+[Yy] -o [Yy][Ee][Ss] )
+continue
+;;
+case $option in
+[Nn] -o [Nn][Oo] )
+break
+;;
+esac
+done
+echo "thanks for using the service"
+
