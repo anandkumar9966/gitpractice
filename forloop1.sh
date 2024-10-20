43b4b0752a203d48a7de840f1876ef263ce3f170
Anand kumar <movidianand123@gmail.com>
2024-10-20 14:06:13 +0530

    forloop12345

--- forloop1.sh
+++ forloop1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+count=0
 for fname in *
 do
 if [ -f $fname ] ; then
-echo " all files displayed successfully $fname"
+echo "$fname"
+let count++
 fi
 done
+echo " the total number of files are:$count"
+ 
