0fdb2e9d2368342e4598c0755f89abc0631b4760
Anand kumar <movidianand123@gmail.com>
2024-10-20 23:33:37 +0530

    arrays1

--- arrays.sh
+++ arrays.sh
@@ -2,5 +2,14 @@
 courses=( "java" "python" "aws" "linux")
 echo ${courses[0]}
 echo ${courses[1]}
-echo ${courses[0]}
+echo ${courses[2]}
+echo ${courses[3]}
 echo ${courses[@]}
+echo ${courses[*]}
+echo "the indexes of all the the arrays are" ${!courses[@]}
+echo "the total no of arrays are" ${#courses[@]}
+echo "the length of the first element is" ${#courses[0]}
+
+
+
+
