d2f1639c3adfb180d381ce7522516a24866ef7e3
Anand kumar <movidianand123@gmail.com>
2024-10-21 00:43:04 +0530

    mysqljust

--- mysqlpractice.sh
+++ mysqlpractice.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-if [ $(id -u) -ne 0 ]; then
+if [ $(sudo id -u) -ne 0 ]; then
 echo " ERROR: please run the script with root user to install mysql"
 exit 1
 else
 echo "mysql installing...please wait"
 fi
 yum install mysql -y
+echo
+if [ $? -ne 0 ]; then
+echo " mysql installation has failed "
+exit 1
+else
+echo "mysql has installed successfully"
+fi
+
+
